d945064de3dbc9699f64cb205793b61cb5f8df4d
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-16 23:02:40 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,13 +8,13 @@ module Players
       if board.turn_count == 0
         move = "5"
 
-      elsif board.count == 1 && !board.taken?(5)
+      elsif board.turn_count == 1 && !board.taken?(5)
         move = "5"
 
       elsif board.count == 1 && board.taken?(5)
         move = [1, 3, 7, 9].detect {|i| !board.taken?(i)}.to_s
 
-      elsif board.count >= 2
+      elsif board.turn_count >= 2
         move = board.cells.detect {|i| !board.taken?(i)}.to_s
       end
     end
