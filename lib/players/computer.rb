590ed8b4a68f2dc61155a2095db60bfdaf3402c1
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-16 23:54:47 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,7 +15,7 @@ module Players
         move = [1, 3, 7, 9].detect {|i| !board.taken?(i)}.to_s
 
       elsif board.turn_count >= 2
-        binding.pry
+        # binding.pry
         move = [0..9].detect {|i| !board.taken?(i)}.to_s
         # move = board.cells.detect
       end
